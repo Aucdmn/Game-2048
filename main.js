20b8e42689b575a131d89f80cf611e6321fbe1d8
Aucdmn <auop129@163.com>
2020-07-20 14:16:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -169,6 +169,10 @@ document.addEventListener('touchstart', function(event) {
     starty = event.touches[0].pageY;
 });
 
+document.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+});
+
 document.addEventListener('touchend', function(event) {
     endx = event.changedTouches[0].pageX;
     endy = event.changedTouches[0].pageY;
