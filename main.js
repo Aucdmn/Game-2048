5441f597ac186af14753c6afeeb325fbec0ea934
Aucdmn <auop129@163.com>
2020-07-20 14:22:44 +0800

    touch-action：none

--- main.js
+++ main.js
@@ -169,9 +169,9 @@ document.addEventListener('touchstart', function(event) {
     starty = event.touches[0].pageY;
 });
 
-document.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-});
+// document.addEventListener('touchmove', function(event) {
+//     event.preventDefault();
+// });
 
 document.addEventListener('touchend', function(event) {
     endx = event.changedTouches[0].pageX;
